32f2ddb1de462d87141aa16289bb8e66831ff94e
HarshWavikar <harshwavikar17@gmail.com>
2024-06-12 11:41:54 +0530

    Designed OnBoarding Screens, page indicator and added dark and light app logo.

--- app/src/main/java/com/codewithharsh/newsapp_updated/MainActivity.kt
+++ app/src/main/java/com/codewithharsh/newsapp_updated/MainActivity.kt
@@ -11,15 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.codewithharsh.newsapp_updated.presentation.onboarding.OnBoardingScreen
 import com.codewithharsh.newsapp_updated.ui.theme.NewsApp_UpdatedTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             NewsApp_UpdatedTheme {
-
+                OnBoardingScreen()
             }
         }
     }
